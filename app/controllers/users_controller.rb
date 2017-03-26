eb699590ce9b9c7c399770193670b0c16818f2d3
Manhnv <manh.nguyenvan@pixta.co.jp>
2017-03-26 09:40:57 +0700

    Lately_goal

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,14 @@
+require 'pry'
 class UsersController < UserBaseController
 
   # GET /users/1
   # GET /users/1.json
   def show
     user = User.find(params[:id])
+    @is_my_page = user.id == current_user.id
     @goals = user.goals.active
     @group = user.groups
+    @late_goal = Goal.lazy_goal(user.id)
   end
 
  
--- app/models/goal.rb
+++ app/models/goal.rb
@@ -5,7 +5,7 @@ class Goal < ApplicationRecord
   validates :status, :inclusion => {:in => STATUS_OPTIONS}
 
   scope :active, -> { where(status: "in_progress" ) }
-
+  scope :goal_of,-> (id) { where(owner_id: id) }
   # add statemachine
   state_machine :status, :initial => :in_progress do
     event :finish do
@@ -18,5 +18,17 @@ class Goal < ApplicationRecord
     	self.current_quantity += activity.quantity
     end
     self.save!
+  end
+
+  def self.lazy_goal(user_id)
+    goal_of(user_id).select do |goal|
+      goal.percent_of_time < goal.percent_of_quantity
+    end
+  end
+  def percent_of_time
+    (deadline.to_f - Time.now.to_f)/(deadline.to_f - created_at.to_f)
+  end
+  def percent_of_quantity
+    (target.to_f - current_quantity.to_f)/target.to_f
   end
 end
