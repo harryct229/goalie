59b1e77f6bec55baad555e62fd9b63b011a5b080
Manhnv <manh.nguyenvan@pixta.co.jp>
2017-03-26 07:57:29 +0700

    Fix permisstion to permission

--- app/controllers/user_base_controller.rb
+++ app/controllers/user_base_controller.rb
@@ -1,7 +1,7 @@
 class UserBaseController < ApplicationController
   layout "user_layout"
-  before_action :check_permisstion 
-  def check_permisstion
+  before_action :check_permission
+  def check_permission
   	return redirect_to new_user_session_url unless current_user
   end
 end
